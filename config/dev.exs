286b1d637f4a9d5e5d933eff2e319d5599cea0e1
Alex Pricinoc <alex.pricinoc@gmail.com>
2022-07-07 17:24:36 +0100

    Add Tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :readmark, ReadmarkWeb.Endpoint,
   secret_key_base: "xds8zUnKohArIddlTgROTmG6sRd5toJD7yoyuQMhI0YYsJsHjBbUVahKIruTMx9c",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
