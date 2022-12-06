005b1e9b47fa1aa6f357956d9d5684fcaed121a9
Alex Pricinoc <alex.pricinoc@gmail.com>
2022-12-06 15:46:43 +0000

    mix phx.gen.auth Accounts User users

--- lib/readmark_web/router.ex
+++ lib/readmark_web/router.ex
@@ -1,6 +1,8 @@
 defmodule ReadmarkWeb.Router do
   use ReadmarkWeb, :router
 
+  import ReadmarkWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,27 +10,11 @@ defmodule ReadmarkWeb.Router do
     plug :put_root_layout, {ReadmarkWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
   end
 
   pipeline :api do
     plug :accepts, ["json"]
-  end
-
-  scope "/", ReadmarkWeb do
-    pipe_through :browser
-
-    live_session :default, on_mount: [ReadmarkWeb.Sidebar] do
-      live "/", HomeLive, :index
-      live "/notes", NotesLive, :index
-      live "/notes/:id", NotesLive, :show
-
-      live "/bookmarks", BookmarksLive, :index
-      live "/bookmarks/new", BookmarksLive, :new
-      live "/bookmarks/:id", BookmarksLive, :show
-      live "/bookmarks/:id/edit", BookmarksLive, :edit
-
-      live "/settings", SettingsLive, :index
-    end
   end
 
   # Other scopes may use custom stacks.
@@ -37,7 +23,7 @@ defmodule ReadmarkWeb.Router do
   # end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:test, :dev_routes) do
+  if Application.compile_env(:readmark, :dev_routes) do
     # If you want to use the LiveDashboard in production, you should put
     # it behind authentication and allow only admins to access it.
     # If your application does not have an admins-only section yet,
@@ -52,6 +38,54 @@ defmodule ReadmarkWeb.Router do
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
-end
 
-# TODO: Authentication routes
+  ## Authentication routes
+
+  scope "/", ReadmarkWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{ReadmarkWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", ReadmarkWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{ReadmarkWeb.UserAuth, :ensure_authenticated}, ReadmarkWeb.Sidebar] do
+      live "/", HomeLive, :index
+      live "/notes", NotesLive, :index
+      live "/notes/:id", NotesLive, :show
+
+      live "/bookmarks", BookmarksLive, :index
+      live "/bookmarks/new", BookmarksLive, :new
+      live "/bookmarks/:id", BookmarksLive, :show
+      live "/bookmarks/:id/edit", BookmarksLive, :edit
+
+      live "/settings", SettingsLive, :index
+      live "/settings/confirm_email/:token", SettingsLive, :confirm_email
+      live "/settings/change_email", SettingsLive, :change_email
+      live "/settings/change_display_name", SettingsLive, :change_display_name
+      live "/settings/change_password", SettingsLive, :change_password
+    end
+  end
+
+  scope "/", ReadmarkWeb do
+    pipe_through [:browser]
+
+    delete "/users/log_out", UserSessionController, :delete
+
+    live_session :current_user,
+      on_mount: [{ReadmarkWeb.UserAuth, :mount_current_user}] do
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+    end
+  end
+end
