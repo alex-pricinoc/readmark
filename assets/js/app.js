dda3d5ada3d7a1542a5f691feb2bf18fa1353635
Alex Pricinoc <alex.pricinoc@gmail.com>
2023-01-08 09:52:17 +0000

    Use dprint instead of prettier

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 import "phoenix_html"
 import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
-import { registerTopbar, registerGlobalEventHandlers } from "./events"
+import { registerGlobalEventHandlers, registerTopbar } from "./events"
 import hooks from "./hooks"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
