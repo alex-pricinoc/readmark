869687ce4cd34399ac827ac129af1838cd0fca89
Alex Pricinoc <alex.pricinoc@gmail.com>
2023-01-18 10:29:14 +0000

    Simplify Readability module
    - use `System.cmd` which is an abstraction over `Port`
    - refactor Article fetcher to use the new Readability module

--- lib/readmark/workers/article_fetcher.ex
+++ lib/readmark/workers/article_fetcher.ex
@@ -1,7 +1,11 @@
 defmodule Readmark.Workers.ArticleFetcher do
   use GenServer
 
-  alias __MODULE__.Impl
+  require Logger
+
+  alias Readmark.Repo
+  alias Readmark.{Bookmarks, Readability}
+  alias Bookmarks.{Article, Bookmark}
 
   @name __MODULE__
 
@@ -11,16 +15,16 @@ defmodule Readmark.Workers.ArticleFetcher do
   def start_link(_), do: GenServer.start_link(__MODULE__, nil, name: @name)
 
   @doc """
-  Fetches the article for a given bookmark and updates it.
+  Fetches the article of a bookmark.
   """
-  def fetch_bookmark_article(bookmark), do: GenServer.cast(@name, {:fetch, bookmark})
+  def fetch_bookmark(%Bookmark{} = bookmark), do: GenServer.cast(@name, {:fetch, bookmark})
 
   @doc """
   Retrieves an existing article from the db or fetches and inserts a new one.
 
   Returns an `Article` struct or `nil`.
   """
-  def get_or_fetch_article(url), do: GenServer.call(@name, {:fetch, url})
+  def get_or_fetch_article(url) when is_binary(url), do: GenServer.call(@name, {:fetch, url})
 
   # Server (callbacks)
 
@@ -31,13 +35,43 @@ defmodule Readmark.Workers.ArticleFetcher do
 
   @impl true
   def handle_cast({:fetch, bookmark}, state) do
-    Impl.fetch_bookmark_article(bookmark)
+    bookmark = Repo.preload(bookmark, :articles)
+
+    with %Article{} = article <- get_or_fetch_article(bookmark.url),
+         {:ok, _bookmark} <- Bookmarks.update_bookmark(bookmark, %{"articles" => [article]}) do
+      :ok
+    else
+      error ->
+        Logger.error("Unable to save bookmark #{inspect(error)}")
+        {:error, error}
+    end
 
     {:noreply, state}
   end
 
   @impl true
   def handle_call({:fetch, url}, _from, state) do
-    {:reply, Impl.get_or_fetch_article(url), state}
+    article = Repo.get(Article, url) || maybe_insert_article(url, Readability.summarize(url))
+
+    {:reply, article, state}
+  end
+
+  defp maybe_insert_article(url, {:ok, attrs}) do
+    %Article{}
+    |> Article.changeset(attrs)
+    |> Repo.insert()
+    |> case do
+      {:ok, article} ->
+        article
+
+      {:error, error} ->
+        Logger.error("Unable to insert article: #{inspect(error)}")
+        Repo.get(Article, url)
+    end
+  end
+
+  defp maybe_insert_article(_url, {:error, error}) do
+    Logger.error("Unable to summarize article: #{inspect(error)}")
+    nil
   end
 end
