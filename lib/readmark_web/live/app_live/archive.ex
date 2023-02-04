d240859c1065b88e3fd1009c4e4a56c07ed4162e
Alex Pricinoc <alex.pricinoc@gmail.com>
2023-02-04 12:43:09 +0000

    Refactor and format

--- lib/readmark_web/live/app_live/archive.ex
+++ lib/readmark_web/live/app_live/archive.ex
@@ -6,6 +6,4 @@ defmodule ReadmarkWeb.AppLive.Archive do
 
   @impl true
   def assign_title(socket, :index), do: assign(socket, :page_title, "Archived bookmarks")
-  def assign_title(socket, :new), do: assign(socket, :page_title, "New bookmark")
-  def assign_title(socket, :edit), do: assign(socket, :page_title, "Edit bookmark")
 end
